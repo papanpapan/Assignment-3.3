d797509c15c412852ea1a544cfde4d7497344adf
papanpapan <samim.skmolla@gmail.com>
2017-04-05 12:33:19 +0530

    Assignment3.3
    Assignment3.3

---
+++ src/studentresult.java
@@ -0,0 +1,240 @@
+
+public class studentresult {
+
+	
+	
+		static String uniname="WBUT";
+		static String sj="C";
+		static String sj1="C++";
+		static String sj2="Java";
+		static String sj3="O/S";
+		 static String clz="ksms";
+		 String name="Faruk Molla";
+		 String course="BCA";
+		 String adress="City:Barasat,P.S:Barasat,Pin:743455";
+		 
+		 
+		 
+		// static String uninamed="WBUT";
+			static String suj="Bangla";
+			static String suj1="Math";
+			static String suj2="Accounts";
+			static String suj3="O/S";
+			 static String clz1="IITS";
+			 String course1="BBA";
+			 String name1="Raja Roy";
+			 
+			 
+			 
+				static String sbj="c";
+		         static String clz2="Adamas";
+				 String name3="Ajay Das";
+			
+		 
+		 
+		 
+		 
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method 
+			     studentresult obj=new studentresult();
+				
+				System.out.println("University name is:"+obj.uniname+"\n");
+				System.out.println("collage name is:"+obj.clz+"\n"); 
+				
+				
+				int rol=1000401;
+			    System.out.println("Roll Nomber:"+rol+"\n"); 
+			    
+			    
+			    int reg=1000001;
+			    System.out.println("Registation Nomber:"+reg+"\n");
+			    System.out.println("Student name is:"+obj.name1+"\n");
+			    
+			    System.out.println("Course:"+obj.course1+"\n");
+			    
+			    
+				System.out.println("Student Address:"+obj.adress+"\n");
+				
+				
+				
+				
+				int a=77;
+				int a1=35;
+				int a2=55;
+				int a3=32;
+				System.out.print("Subject:"+"\t"+"Marks"+"\n");
+				System.out.print(obj.suj+"==>    "+"\t"+a+"\n");
+				System.out.print(obj.suj1+"==>  "+"\t"+a1+"\n");
+				System.out.print(obj.suj2+"==>  "+"\t"+a2+"\n");
+				System.out.print(obj.suj3+"==>  "+"\t"+a3+"\n");
+				int total;
+				total=a+a1+a2+a3;
+				System.out.println("Total marks is:"+total+"\n");
+				int par;
+				par=total/4;
+				System.out.println("Persentance of the total marks:"+par+"%"+"\n"); 
+				
+				
+				if(par>=70 & par<=100){
+					System.out.println("Grade A"+"\n");
+				}
+				if(par>60 & par<=70){
+					System.out.println("Grade B"+"\n");
+							
+				}
+				
+				if(par>=45 & par<=60){
+					System.out.println("Grade C"+"\n");
+							
+				} 
+				
+				
+				
+				
+				System.out.println("====================================================================================="+"\n");
+				
+				
+				
+				
+				
+				
+				System.out.println("University name is:"+obj.uniname+"\n");
+				System.out.println("collage name is:"+obj.clz+"\n"); 
+				
+				
+				int rol2=1000402;
+			    System.out.println("Roll Number:"+rol2+"\n"); 
+			    
+			    
+			    int reg2=1000002;
+			    System.out.println("Registation Number:"+reg2+"\n");
+			    
+				System.out.println("Student name is:"+obj.name+"\n");
+				String course2="BCA";
+				 String adress1="City:Barasat,P.S:Barasat,Pin:743455";
+				 System.out.println("Course:"+course2+"\n");
+				    
+				 System.out.print("Student Address:"+adress1+"\n");
+					
+				
+				int ay=78;
+				int a1y=45;
+				int a2y=45;
+				int a3y=34;
+				System.out.println("Subject:"+"\t"+"Marks"+"\n");
+				System.out.println(obj.sj+"==>    "+"\t"+ay+"\n");
+				System.out.println(obj.sj1+"==>  "+"\t"+a1y+"\n");
+				System.out.println(obj.sj2+"==>  "+"\t"+a2y+"\n");
+				System.out.println(obj.sj3+"==>  "+"\t"+a3y+"\n");
+				int total2=ay+a1y+a2y+a3y;
+				System.out.println("Total marks is:"+total2+"\n");
+				int par2=total2/4;
+				System.out.println("Persentance of the total marks:"+par2+"%"+"\n");
+				
+				
+				if(par2>=70 & par2<=100){
+					System.out.println("Grade A"+"\n");
+				}
+				if(par2>60 & par2<=70){
+					System.out.println("Grade B"+"\n");
+							
+				}
+				
+				if(par2>=45 & par2<=60){
+					System.out.print("Grade C"+"\n");
+							
+				} 
+				
+				
+				
+				
+				
+				System.out.println("====================================================================================="+"\n");
+
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+						
+						System.out.println("University name is:"+obj.uniname+"\n");
+						System.out.print("collage name is:"+obj.clz1+"\n"); 
+						
+						
+						int rol1=1000403;
+					    System.out.println("Roll Nomber:"+rol1+"\n"); 
+					    
+					    
+					    int reg1=1000004;
+					    System.out.println("Registation Nomber:"+reg1+"\n");
+					    
+					    
+					    
+						System.out.println("Student name is:"+obj.name3+"\n");
+						
+						
+						String course5="BBA";
+						 String adress5="City:Barasat,P.S:Barasat,Pin:743455";
+						
+						
+						 System.out.println("Course:"+course5+"\n");
+						    
+						    
+							System.out.print("Student Address:"+adress5+"\n");
+							
+						
+						int ta=36;
+						int ta1=87;
+						int ta2=89;
+						int ta3=32;
+						System.out.println("Subject:"+"\t"+"Marks"+"\n");
+						System.out.println(obj.sj+"==>    "+"\t"+ta+"\n");
+						System.out.println(obj.sj1+"==>  "+"\t"+ta1+"\n");
+						System.out.println(obj.sj2+"==>  "+"\t"+ta2+"\n");
+						System.out.println(obj.sj3+"==>  "+"\t"+ta3+"\n");
+						int totalt;
+						totalt=ta+ta1+ta2+ta3;
+						System.out.println("Total marks is:"+totalt+"\n");
+						int par1t;
+						par1t=totalt/4;
+						System.out.println("Persentance of the total marks:"+par1t+"%"+"\n"); 
+						
+						
+						if(par1t>=70 & par1t<=100){
+							System.out.println("Grade A"+"\n");
+						}
+						if(par1t>60 & par1t<=70){
+							System.out.println("Grade B"+"\n");
+									
+						}
+						
+						if(par1t>=45 & par1t<=60){
+							System.out.println("Grade C"+"\n");
+									
+						} 
+						
+						
+						
+						
+						System.out.println("====================================================================================="+"\n"); 
+						
+				      
+				
+
+						
+
+					
+
+
+
+
+	}
+
+}
